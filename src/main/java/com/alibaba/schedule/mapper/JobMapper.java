b82d14de0e9bd8f0c1dc2ad45e31c99546daf8f8
江湖依旧 <wb-dhy359835@MININT-75VQSAA.hz.ali.com>
2018-01-12 16:11:23 +0800

    commit job mapper

---
+++ src/main/java/com/alibaba/schedule/mapper/JobMapper.java
@@ -0,0 +1,43 @@
+package com.alibaba.schedule.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
+
+import com.alibaba.schedule.domin.JobDO;
+
+
+/**
+ * 
+ * @author dunhanyang
+ *
+ */
+@Repository
+public interface JobMapper {
+
+	@Insert("insert into t_job(gmt_create,gmt_modified,guid,desc_ver,group_id,expect_status,conf,creator)"
+			+ " values(#{job.gmtCreate},#{job.gmtModified},#{job.guid},#{job.desc},#{job.groupId},#{job.expectStatus},#{job.conf},#{job.creator})")
+	public int add(@Param("job") JobDO jobDO);
+	
+	@Select("select * from t_job")
+	@Results({
+	      @Result(property="gmtCreate" , column="gmt_create"),
+	      @Result(property="gmtModified",column="gmt_modified"),
+	      @Result(property="groupId",column="group_id"),
+	      @Result(property="expectStatus",column="expect_status")
+	  })
+	public List<JobDO> selectAll();
+	@Delete("delete from t_job where id=#{id}")
+	public void deleteById(@Param("id") long id);
+	@Update("update t_job set creator=${creator} where id=#{id}")
+	public void updateById(@Param("id") long id,@Param("creator") String creator);
+	@Select("select gmt_create,gmt_modified,guid,desc,group_id,conf,creator from t_job where id=#{id}")
+	public List<JobDO> queryById(@Param("id") long id);
+}
