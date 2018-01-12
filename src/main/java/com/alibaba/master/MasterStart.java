49ecbd299248eb11e2207c6841f70a82577fcf86
wb-cqk360000 <wb-cqk360000@MININT-I6S8FFK.hz.ali.com>
2018-01-12 13:32:26 +0800

    tx

--- src/main/java/com/alibaba/master/MasterStart.java
+++ src/main/java/com/alibaba/master/MasterStart.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan(basePackages={"com.alibaba.schedule.mapper"})
 @ComponentScan(value={"com.alibaba.schedule","com.alibaba.master.*"})
 public class MasterStart implements CommandLineRunner{
-    
+     
 	@Override
 	public void run(String... paramArrayOfString) throws Exception {
 		// todo
